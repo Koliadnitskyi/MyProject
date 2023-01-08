4900b17bcba20c58da1ef6c15109e340f62e450a
Koliadnitskyi Valerii <0164zeos20081@gmail.com>
2023-01-08 13:15:37 +0200

    Concatenation of Array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] nums = {1, 2, 1};
+        int arraySize = nums.length * 2;
+        int[] nums2 = new int[arraySize];
+        int e = 0;
+        for (int r = 0; r < nums2.length; r++) {
+            nums2[r] = nums[e];
+            e++;
+            if (e == nums.length) {
+                e = 0;
+            }
+        }
+        System.out.println(Arrays.toString(nums2));
     }
 }
