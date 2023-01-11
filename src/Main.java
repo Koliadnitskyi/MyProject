d9cc1f964a025bae086bbb790ed24c53bad3d2ff
Koliadnitskyi Valerii <0164zeos20081@gmail.com>
2023-01-11 08:19:00 +0200

    Maximum Number of Words Found in Sentences

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String address = "1.1.1.1";
-        String f = address.replace(".", "[.]");
-        System.out.println(f);
+        String[] sentences = {"alice and bob love leetcode", "i think so too", "this is great thanks very much"};
+        int z = 0;
+        for (int d = 0; d < sentences.length; d++) {
+            String[] words = sentences[d].split("\\s");
+            int q = words.length;
+            if (q>z) z=q;
+        }
+        System.out.println(z);
     }
-} // Defanging an IP Address
+} //Maximum Number of Words Found in Sentences
