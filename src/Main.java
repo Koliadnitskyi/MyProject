6e98f4fc28a142f5b219811539cbfd77e796fff9
Koliadnitskyi Valerii <0164zeos20081@gmail.com>
2023-01-26 21:08:37 +0200

    add working with figures

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
-import java.util.Arrays;
-
 public class Main {
-    public static void main(String[] args) {
-        String[] sentences = {"alice and bob love leetcode", "i think so too", "this is great thanks very much"};
-        int z = 0;
-        for (int d = 0; d < sentences.length; d++) {
-            String[] words = sentences[d].split("\\s");
-            int q = words.length;
-            if (q>z) z=q;
+    private static void totalArea(double[] arrayOfShapesTwo) {
+        double counter = 0.0;
+        for (double arrayOfShape : arrayOfShapesTwo) {
+            counter = counter + arrayOfShape;
         }
-        System.out.println(z);
+        System.out.print("Сумарна площа усіх фігур: " + counter + ".");
     }
-} //Maximum Number of Words Found in Sentences
+
+    public static void main(String[] args) {
+        double[] arrayOfShapes = new double[3];
+        Circle circle = new Circle(12);
+        arrayOfShapes[0] = circle.countArea();
+        Square square = new Square(10);
+        arrayOfShapes[1] = square.countArea();
+        Triangle triangle = new Triangle(12, 10);
+        arrayOfShapes[2] = triangle.countArea();
+        totalArea(arrayOfShapes);
+    }
+}
